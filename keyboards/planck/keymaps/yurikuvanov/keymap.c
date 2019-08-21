e94ab00cb9f0e8c6dd78587f680a9c48235e867e
Yuri Kuvanov <lopakhin@gmail.com>
2019-08-21 18:24:11 +0900

    Add US keymap

--- keyboards/planck/keymaps/yurikuvanov/keymap.c
+++ keyboards/planck/keymaps/yurikuvanov/keymap.c
@@ -25,12 +25,14 @@ enum planck_layers {
   _JIS,
   _LOWER,
   _RAISE,
+  _US,
   _PLOVER,
   _ADJUST
 };
 
 enum planck_keycodes {
   JIS = SAFE_RANGE,
+  US,
   PLOVER,
   BACKLIT,
   WN_SCLN,
@@ -60,7 +62,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
     KC_ESC,  KC_LGUI, KC_LALT, KC_LCTL, LT(LOWER,JP_MHEN),KC_SPC,KC_ENT,LT(RAISE,JP_HENK),KC_LEFT, KC_DOWN, KC_UP,KC_RGHT
 ),
+ /*  US
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | ; :  | -  _ |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  | , <  | . >  | / ?  |SHIFT |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |  GUI | Alt  | Ctrl |Lower | Space|Enter |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
 
+ [_US] = {
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_MINS,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
+    KC_ESC,  KC_LGUI, KC_LALT, KC_LCTL, LT(LOWER,KC_LANG2),KC_SPC,KC_ENT,LT(RAISE,KC_LANG1),KC_LEFT, KC_DOWN, KC_UP,KC_RGHT
+ },
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |半/全 |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | DEL  |
@@ -127,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, US, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, JIS,     _______,  _______, _______,  _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
@@ -152,6 +171,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         print("mode just switched to qwerty and this is a huge string\n");
         set_single_persistent_default_layer(_JIS);
+      }
+      return false;
+      break;
+  switch (keycode) {
+    case US:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_US);
       }
       return false;
       break;
