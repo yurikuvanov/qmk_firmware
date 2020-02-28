a1d4468bc90f86a5d97e0fc4dff85752ff20be9d
yurikuvanov <lopakhin@gmail.com>
2020-02-29 02:30:11 +0900

    Update keymap.c

--- keyboards/planck/keymaps/yurikuvanov/keymap.c
+++ keyboards/planck/keymaps/yurikuvanov/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_MINS,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-    KC_ESC,  _______, KC_LALT, KC_LCMD, LOWERUS, KC_SPC,  KC_ENT,  RAISEUS, KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT
+    KC_ESC,  _______, KC_LALT, KC_LGUI, LOWERUS, KC_SPC,  KC_ENT,  RAISEUS, KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT
 ),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -157,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * |      | Reset|      |      |EEPRST|      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |Aud on|Audoff|AGnorm|AGswap| JIS  | US   |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -167,7 +167,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, US, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+    _______, RESET,   DEBUG,   RGB_TOG, EEP_RST, RGB_HUI, RGB_HUD, US, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, JIS,     US     ,  _______, _______,  _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, KC_TRNS
